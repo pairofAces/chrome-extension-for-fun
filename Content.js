679d0989fe294b720cbf7e071709a691370e53df
pairofAces <karanchauhan16@gmail.com>
2021-06-03 17:13:32 -0400

    added image URLs as data for chrome extension

--- Content.js
+++ Content.js
@@ -1,4 +1,10 @@
 // create an array to act as the list of URLs(pics) of John Snow
 let johnSnowImages = [
     // insert URLs here
-]
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqkkUj9fz7tseoQRgBfwxWfREOeq2cwHSeng&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnu0XhRwiHtyKEkJ8xkKpZWn7ME4YXC629KA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTH5OF4wbIDDpP3D-aRJ_234iO-0LcInDQjA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTukG8cnumPFm4a_iV87ZRs53Vpl_sTPTVCgA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZGqEUT2BOR3CxdjWwGmoACtFAw7ueOt0BBw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh6W2RSJ3F-x5gRYny3Go9Y6rjSznwf6EehQ&usqp=CAU",
+];
